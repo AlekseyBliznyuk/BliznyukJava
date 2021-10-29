4bc2919eb13605df691feee5d4fe0aa74ef213ff
AlekseyBliznyuk <58771284+AlekseyBliznyuk@users.noreply.github.com>
2021-10-29 12:24:25 +0300

    Update BliznyukJava_Task2.java

--- BliznyukJava_Task2/src/bliznyukjava_task2/BliznyukJava_Task2.java
+++ BliznyukJava_Task2/src/bliznyukjava_task2/BliznyukJava_Task2.java
@@ -1,10 +1,11 @@
+/*результат программы будет отображён в виде двух строк: 
+первая - результат сравнения str и str2,
+вторая - результат сравнения str1 и str2
+*/
 package bliznyukjava_task2;
-
-import java.util.Scanner;
 
 public class BliznyukJava_Task2 {
     public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
         String str="Вячеслав";
         String str1="вячеслав";
         String str2="Вячеслав";       
