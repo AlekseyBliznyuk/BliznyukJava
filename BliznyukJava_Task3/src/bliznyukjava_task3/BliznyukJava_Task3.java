d8afa7f2c7b0bc7c3181639f823e7e1d463c656a
AlekseyBliznyuk <58771284+AlekseyBliznyuk@users.noreply.github.com>
2021-10-29 12:25:12 +0300

    Update BliznyukJava_Task3.java

--- BliznyukJava_Task3/src/bliznyukjava_task3/BliznyukJava_Task3.java
+++ BliznyukJava_Task3/src/bliznyukjava_task3/BliznyukJava_Task3.java
@@ -1,17 +1,25 @@
 package bliznyukjava_task3;
-
+import java.util.Scanner;
 
 public class BliznyukJava_Task3 {
 
     public static void main(String[] args) {
-        int[] a = {1,2,3,4,5,6,7,8,9};
+        Scanner input = new Scanner(System.in);
+        System.out.println("Размер массива:");
+        int size = input.nextInt();
+        int[] a = new int[size];
+        System.out.println("Введите элементы массива:");
+        for (int i = 0;i < size; i++)
+        {
+            a[i] = input.nextInt();
+        }
+        System.out.println("Итог");
         for(int i = 0; i < a.length; i++)
         {
             if(a[i]%3==0)
-            {
+            {               
                 System.out.println(a[i]);
             }
         }
     }
-    
 }
